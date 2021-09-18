2c279ed490ccc392e504d0c69305d21cbb547394
Yanisa Hattasarn <87184757+tmrwithu@users.noreply.github.com>
2021-09-18 15:38:36 +0700

    add count

--- main.js
+++ main.js
@@ -2,7 +2,26 @@
 const popcat = document.querySelector("#popcat");
 const btn = document.querySelector("#btn");
 const popCount = document.querySelector("#count");
+popCount.innerText = syncCook();
 
+function syncCook() {
+    let popCookie = document.cookie.split(";").filter(m => m.startsWith('pop-cook'));
+    let count = null;
+    if (!popCookie.length) {
+        document.cookie = 'pop-cook=0;';
+        count = 0;
+    } else {
+        count = parseInt(popCookie[0].substr(9));
+    }
+    return count;
+}
+function addCook() {
+    console.log("add");
+    count = syncCook();
+    document.cookie = `pop-cook=${count++};`;
+    console.log(count);
+    return count;
+}
 // The two images of the POP CAT
 const openMouthImg = "./images/DOW2.png";
 const closeMouthImg = "./images/DOW1.png";
@@ -16,21 +35,21 @@ btn.addEventListener("mousedown", openMouth);
 btn.addEventListener("mouseup", closeMouth);
 
 // Event Handers (Touch Screens)
-btn.addEventListener("touchstart", function(e) {
+btn.addEventListener("touchstart", function (e) {
     e.preventDefault();
     openMouth();
 })
 
-btn.addEventListener("touchend", function(e) {
-    
+btn.addEventListener("touchend", function (e) {
+
     e.preventDefault();
     closeMouth();
 })
 
 // The functions which will perform the cool stuff
 function openMouth() {
-    let count = parseInt(popCount.innerText);
-    popCount.innerText = count+1;
+    
+    popCount.innerText = addCook();
     popcat.src = openMouthImg;
     openMouthSound.play();
 }
